bf9ab77f75aa731c0ec32d6ad6ce8fe5cae57cdd
siam3h <philosiama@gmail.com>
2024-12-20 22:52:39 +0300

    Update project files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config'; 
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
+//import react from '@astro/react';
 
 export default defineConfig({
-  integrations: [tailwind(), compress()],
+  integrations: [tailwind(), compress() ]//react()],
 });
